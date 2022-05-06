3214980d995f92085b000eeb8315ea21a636fe92
pmcorrea <pmcorrea@me.com>
2022-05-06 09:10:23 -0400

    added binary search category

---
+++ Binary Search/Find target.js
@@ -0,0 +1,20 @@
+/*
+Problem Statement:
+    This is some sample text.
+
+Clarifications and Assumptions:
+    This is some sample text.
+
+Test Case:
+    () => []
+
+Notes:
+    This is some sample text.
+
+*/
+
+// Solution #1 - O() time | O() space:
+export const first = () => {}
+
+// Solution #2 - O() time | O() space:
+export const second = () => {}
